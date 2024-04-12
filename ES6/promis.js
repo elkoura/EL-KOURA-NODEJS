373b9b12ec8dc00d02674e9557c2c5ba83c3bd1f
elkoura <elkourafouad@gmail.com>
2024-04-12 08:28:48 +0200

    cc

---
+++ ES6/promis.js
@@ -0,0 +1,9 @@
+const asyncfunction=() => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      
+    }, const random=math.random();
+    if (random>0,)
+  )
+  }
+}
---
+++ app.js/appp.js
@@ -0,0 +1,45 @@
+/*let array=[1, 2, 3]
+console.log(array)
+let copy=[...array,4,5,6]
+console.log(copy)
+let[first, sec]=copy
+console.log(first)
+let c=copy[2]
+console.log(c)
+
+///objet
+let obj={
+  message: "hello word"
+
+}
+console.log(obj)
+console.log(obj.message)
+
+obj.type="success!"
+console.log(obj)
+let objcopy={...obj, id :5}
+console.log(objcopy)
+
+const {type,id}=objcopy
+
+console.log(type, id)
+
+function varTest(){
+  let x=1
+  if(true){
+    let x=2
+    console.log(X)
+  }
+}
+*/
+//
+
+/*const array=[1, 2, 3, 4, 5, 6]
+array.forEach(value => console.log(value))
+  */
+
+const a = (parm, param2) =>{
+  const sum=param+param2
+  return sum
+  console.log(a (2,2))
+}
